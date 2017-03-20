628af8f54efe6abbf2f30888fe69eada6c169b7c
aocneanu <aocneanu@gmail.com>
2017-03-20 21:22:49 +0000

    Apply fixes from StyleCI

--- src/Http/Requests/ValidateTutorialRequest.php
+++ src/Http/Requests/ValidateTutorialRequest.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ValidateTutorialRequest extends FormRequest
 {
-
     public function authorize()
     {
         return true;
@@ -17,18 +16,16 @@ class ValidateTutorialRequest extends FormRequest
         $tutorial = $this->route('tutorial');
 
         if ($this->_method == 'PATCH') {
-
             return [
 
                 'permission_id' => 'required',
-                'element'       => 'required|unique:tutorials,element,' . $tutorial->id . ',id',
+                'element'       => 'required|unique:tutorials,element,'.$tutorial->id.',id',
                 'title'         => 'required',
                 'content'       => 'required',
                 'placement'     => 'required',
                 'order'         => 'numeric',
             ];
         } else {
-
             return [
 
                 'permission_id' => 'required',
