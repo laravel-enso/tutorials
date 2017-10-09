04bff4e2fedd054c88430ea563a1dce25f541bca
mishu <mishu@xtelecom.ro>
2017-10-09 13:56:26 +0300

    refactor error handling in js

--- src/resources/assets/js/pages/enso/system/tutorials/Edit.vue
+++ src/resources/assets/js/pages/enso/system/tutorials/Edit.vue
@@ -33,7 +33,9 @@
 				this.form = response.data.form;
 				this.role = response.data.role;
 				this.initialised = true;
-			});
+			}).catch(error => {
+				this.handleError(error);
+            });
 		}
 	};
 
