f76dc5f4110e8839bd481f3d5b3bc35e14cd7de4
mishu <mishu@xtelecom.ro>
2017-12-20 08:57:07 +0200

    adds .styleci.yml; updates composer.json for enso/helpers

--- src/app/Models/Tutorial.php
+++ src/app/Models/Tutorial.php
@@ -3,13 +3,10 @@
 namespace LaravelEnso\TutorialManager\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use LaravelEnso\Helpers\Traits\FormattedTimestamps;
 use LaravelEnso\PermissionManager\app\Models\Permission;
 
 class Tutorial extends Model
 {
-    use FormattedTimestamps;
-
     protected $fillable = ['permission_id', 'element', 'title', 'content', 'placement', 'order'];
 
     protected $attributes = ['order' => 1];
