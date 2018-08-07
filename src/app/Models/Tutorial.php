9231f8ffbd4f563afa241280fe81b14e30daa5cd
Jonas De Kegel <jonas@jlss.eu>
2018-08-07 11:01:30 +0200

    Update model to always cast selects to int
    Some database drivers implicitly convert (non-primary?) keys to string.
    
    This broke the selector

--- src/app/Models/Tutorial.php
+++ src/app/Models/Tutorial.php
@@ -10,6 +10,11 @@ class Tutorial extends Model
     protected $fillable = [
         'permission_id', 'element', 'title', 'content', 'placement', 'order_index',
     ];
+    
+    protected $casts = [
+      'permission_id' => 'integer',
+      'placement' => 'integer',
+    ];
 
     public function permission()
     {
