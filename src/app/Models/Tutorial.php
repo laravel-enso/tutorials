2302815e965052e4779441baec8bac988aa7a84c
mishu <mishu@xtelecom.ro>
2018-08-11 14:21:06 +0300

    adds activity log

--- src/app/Models/Tutorial.php
+++ src/app/Models/Tutorial.php
@@ -3,13 +3,20 @@
 namespace LaravelEnso\TutorialManager\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use LaravelEnso\ActivityLog\app\Traits\LogActivity;
 use LaravelEnso\PermissionManager\app\Models\Permission;
 
 class Tutorial extends Model
 {
+    use LogActivity;
+
     protected $fillable = [
         'permission_id', 'element', 'title', 'content', 'placement', 'order_index',
     ];
+
+    protected $loggableLabel = 'title';
+
+    protected $loggable = ['title', 'content', 'placement', 'order_index'];
 
     public function permission()
     {
