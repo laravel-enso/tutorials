25c57659ff3432a58dffc1d32fa37bb75cf177f1
mishu <mishu@xtelecom.ro>
2019-10-12 18:22:38 +0300

    updates dependencies; refactors the table for v3; adds TableCache trait on model

--- src/app/Models/Tutorial.php
+++ src/app/Models/Tutorial.php
@@ -3,10 +3,13 @@
 namespace LaravelEnso\Tutorials\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use LaravelEnso\Tables\app\Traits\TableCache;
 use LaravelEnso\Permissions\app\Models\Permission;
 
 class Tutorial extends Model
 {
+    use TableCache;
+
     protected $fillable = [
         'permission_id', 'element', 'title', 'content', 'placement', 'order_index',
     ];
--- src/app/Tables/Builders/TutorialTable.php
+++ src/app/Tables/Builders/TutorialTable.php
@@ -2,18 +2,24 @@
 
 namespace LaravelEnso\Tutorials\app\Tables\Builders;
 
-use LaravelEnso\Tables\app\Services\Table;
+use Illuminate\Database\Eloquent\Builder;
+use LaravelEnso\Tables\app\Contracts\Table;
 use LaravelEnso\Tutorials\app\Models\Tutorial;
 
-class TutorialTable extends Table
+class TutorialTable implements Table
 {
-    protected $templatePath = __DIR__.'/../Templates/tutorials.json';
+    protected const TemplatePath = __DIR__.'/../Templates/tutorials.json';
 
-    public function query()
+    public function query(): Builder
     {
         return Tutorial::selectRaw('
             tutorials.id, permissions.name as permissionName, tutorials.element,
             tutorials.title, tutorials.placement, tutorials.order_index, tutorials.created_at
         ')->join('permissions', 'permissions.id', '=', 'tutorials.permission_id');
+    }
+
+    public function templatePath(): string
+    {
+        return static::TemplatePath;
     }
 }
