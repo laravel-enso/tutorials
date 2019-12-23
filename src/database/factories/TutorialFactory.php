1cd97ef7bede2a9d6fce9726e5773f631329e2f5
reza <raftx24@gmail.com>
2019-12-23 11:24:29 +0330

    upgrades to php7.4

--- src/database/factories/TutorialFactory.php
+++ src/database/factories/TutorialFactory.php
@@ -5,15 +5,11 @@ use LaravelEnso\Permissions\app\Models\Permission;
 use LaravelEnso\Tutorials\app\Enums\Placement;
 use LaravelEnso\Tutorials\app\Models\Tutorial;
 
-$factory->define(Tutorial::class, function (Faker $faker) {
-    return [
-        'permission_id' => function () {
-            return factory(Permission::class)->create()->id;
-        },
-        'element' => $faker->word,
-        'title' => $faker->word,
-        'content' => $faker->sentence,
-        'placement' => Placement::keys()->random(),
-        'order_index' => $faker->randomNumber(2),
-    ];
-});
+$factory->define(Tutorial::class, fn(Faker $faker) => [
+    'permission_id' => fn() => factory(Permission::class)->create()->id,
+    'element' => $faker->word,
+    'title' => $faker->word,
+    'content' => $faker->sentence,
+    'placement' => Placement::keys()->random(),
+    'order_index' => $faker->randomNumber(2),
+]);
