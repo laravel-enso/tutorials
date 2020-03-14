3f31e37e10813679a77ae969e1723bd56ffdeeac
mishu <mishu@xtelecom.ro>
2020-03-14 18:39:14 +0200

    removes Types from migration

--- src/database/migrations/2017_01_01_136000_create_structure_for_tutorials.php
+++ src/database/migrations/2017_01_01_136000_create_structure_for_tutorials.php
@@ -1,21 +1,20 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForTutorials extends Migration
 {
     protected $permissions = [
-        ['name' => 'system.tutorials.tableData', 'description' => 'Get table data for tutorials', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.tutorials.exportExcel', 'description' => 'Export excel for tutorials', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.tutorials.initTable', 'description' => 'Init table data for tutorials', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.tutorials.create', 'description' => 'Create tutorial', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'system.tutorials.edit', 'description' => 'Edit tutorial', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'system.tutorials.index', 'description' => 'Show tutorials index', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'system.tutorials.store', 'description' => 'Store newly created tutorial', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'system.tutorials.update', 'description' => 'Update edited tutorial', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'system.tutorials.load', 'description' => 'Load tutorial', 'type' => Types::Read, 'is_default' => true],
-        ['name' => 'system.tutorials.destroy', 'description' => 'Delete tutorial', 'type' => Types::Write, 'is_default' => false],
+        ['name' => 'system.tutorials.tableData', 'description' => 'Get table data for tutorials', 'is_default' => false],
+        ['name' => 'system.tutorials.exportExcel', 'description' => 'Export excel for tutorials', 'is_default' => false],
+        ['name' => 'system.tutorials.initTable', 'description' => 'Init table data for tutorials', 'is_default' => false],
+        ['name' => 'system.tutorials.create', 'description' => 'Create tutorial', 'is_default' => false],
+        ['name' => 'system.tutorials.edit', 'description' => 'Edit tutorial', 'is_default' => false],
+        ['name' => 'system.tutorials.index', 'description' => 'Show tutorials index', 'is_default' => false],
+        ['name' => 'system.tutorials.store', 'description' => 'Store newly created tutorial', 'is_default' => false],
+        ['name' => 'system.tutorials.update', 'description' => 'Update edited tutorial', 'is_default' => false],
+        ['name' => 'system.tutorials.load', 'description' => 'Load tutorial', 'is_default' => true],
+        ['name' => 'system.tutorials.destroy', 'description' => 'Delete tutorial', 'is_default' => false],
     ];
 
     protected $menu = [
